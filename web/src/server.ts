90dd6a4b4fc422e665e49a63a47fa97b6df866fa
Dane Burkland <daneburkland@danes-mbp.attlocal.net>
2020-08-22 22:00:19 -0700

    removes some minikube ish

--- web/src/server.ts
+++ web/src/server.ts
@@ -11,5 +11,4 @@ new GraphQLServer({
   console.log(
     `🚀 Server ready at: http://localhost:4000\n⭐️ See sample queries: http://pris.ly/e/ts/graphql-auth#using-the-graphql-api`,
   )
-  console.log(process.env.REDIS_URL)
 })
